b640eb274d04d91e55dd49ab089617ba0c20acd3
Omar AbdullWahhab <omar.abdullwahhab@gmail.com>
2021-09-12 15:08:41 +0300

    Added Microservice Server

---
+++ P02-VehiclesAPI/microservice-server/src/test/java/com/udacity/microserviceslab/MicroservicesLabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.udacity.microserviceslab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicesLabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
