e63fb77f4d8471e7ffa4f40767e0866deb5718e4
Omar AbdullWahhab <omar.abdullwahhab@gmail.com>
2021-09-20 13:24:59 +0300

    First Review

--- P02-VehiclesAPI/vehicles-api/src/test/java/com/udacity/vehicles/api/CarControllerTest.java
+++ P02-VehiclesAPI/vehicles-api/src/test/java/com/udacity/vehicles/api/CarControllerTest.java
@@ -4,9 +4,7 @@ import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.Matchers.is;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -134,6 +132,20 @@ public class CarControllerTest {
         car.setId(1l);
         mvc.perform(delete(new URI("/cars/"+car.getId()))
                 .accept(MediaType.APPLICATION_JSON_UTF8)).andExpect(status().isNoContent());
+    }
+
+    @Test
+    public void updateCar() throws Exception
+    {
+        Car car = getCar();
+        car.setCondition(Condition.NEW);
+        car.setId(1l);
+
+        mvc.perform(put(new URI("/cars" + car.getId()))
+                                .content(json.write(car).getJson())
+                                .contentType(MediaType.APPLICATION_JSON_UTF8)
+                                .accept(MediaType.APPLICATION_JSON_UTF8))
+                .andExpect(status().isCreated());
     }
 
     /**
